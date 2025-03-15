1d966d9e7bbebce241803e9fd378a8f433130013
Hasan <123514871+imhasandl@users.noreply.github.com>
2025-03-15 17:57:12 +0500

    Integrate RabbitMQ for comment notifications and update Post and Comment models to use UUID for PostedBy

--- sql/schema/002_content_system.sql
+++ sql/schema/002_content_system.sql
@@ -3,7 +3,7 @@ CREATE TABLE posts (
    id UUID PRIMARY KEY,
    created_at TIMESTAMP NOT NULL,
    updated_at TIMESTAMP NOT NULL,
-   posted_by TEXT NOT NULL,
+   posted_by UUID NOT NULL,
    body TEXT NOT NULL,
    likes INT NOT NULL DEFAULT 0,
    views INT NOT NULL DEFAULT 0,
